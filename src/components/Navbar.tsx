2aafead964648633981185960826eb323cdd611e
Cody Epstein <codyepstein@gmail.com>
2024-08-29 18:55:35 -0700

    Added header and picture

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+export function Navbar() {
+	return (
+		<>
+			<nav>
+				{/* <ul className="flex gap-3 font-mono">
+					<li>Home</li>
+					<li>About</li>
+					<li>Projects</li>
+					<li>Contact</li>
+				</ul> */}
+			</nav>
+		</>
+	);
+}
